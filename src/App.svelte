c65e5d7e78ee3e335e6c64adb561b69d41f0032e
Nico Glennon <nicoglennon@icloud.com>
2019-10-01 20:23:46 -0500

    colors cycle

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
     }
   );
 
-  let size = spring(20);
+  let size = spring(25);
 </script>
 
 <style>
@@ -18,35 +18,51 @@
     height: 100%;
   }
   circle {
-    fill: #ff3e00;
+    animation: colorchange 10s infinite; /* animation-name followed by duration in seconds*/
+    /* you could also use milliseconds (ms) or something like 2.5s */
+    -webkit-animation: colorchange 10s infinite; /* Chrome and Safari */
+  }
+
+  @keyframes colorchange {
+    0% {
+      fill: red;
+    }
+    25% {
+      fill: yellow;
+    }
+    50% {
+      fill: blue;
+    }
+    75% {
+      fill: green;
+    }
+    100% {
+      fill: red;
+    }
+  }
+
+  @-webkit-keyframes colorchange /* Safari and Chrome - necessary duplicate */ {
+    0% {
+      background: red;
+    }
+    25% {
+      background: yellow;
+    }
+    50% {
+      background: blue;
+    }
+    75% {
+      background: green;
+    }
+    100% {
+      background: red;
+    }
   }
 </style>
 
-<div style="position: absolute; right: 1em;">
-  <label>
-    <h3>stiffness ({coords.stiffness})</h3>
-    <input
-      bind:value={coords.stiffness}
-      type="range"
-      min="0"
-      max="1"
-      step="0.01" />
-  </label>
-
-  <label>
-    <h3>damping ({coords.damping})</h3>
-    <input
-      bind:value={coords.damping}
-      type="range"
-      min="0"
-      max="1"
-      step="0.01" />
-  </label>
-</div>
-
 <svg
   on:mousemove={e => coords.set({ x: e.clientX, y: e.clientY })}
-  on:mousedown={() => size.set(500)}
-  on:mouseup={() => size.set(20)}>
+  on:mousedown={() => size.set(1000)}
+  on:mouseup={() => size.set(25)}>
   <circle cx={$coords.x} cy={$coords.y} r={$size} />
 </svg>
